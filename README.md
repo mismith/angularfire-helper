7896d2fb8edbab06bb79eb8740c62a59a9efac43
Murray Smith <murray@businessinstincts.ca>
2015-01-15 13:52:08 -0700

    v1.1.0: internal engine improved, test page added, readme docs added

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # AngularFire Helper
 
-Helper service to simplify and improve common AngularFire tasks
+Simplifies common AngularFire interactions by enhancing the flexibility and portability of the `$firebase` service.
 
-# Setup
+
+
+## Setup
 
 1. Include Firebase and AngularFire dependencies, then this library
 
         <script src="//cdn.firebase.com/js/client/2.0.6/firebase.js"></script>
         <script src="//cdn.firebase.com/libs/angularfire/0.9.0/angularfire.min.js"></script>
-        <script src="//rawgit.com/mismith/angularfire-helper/1.0.0/angularfire-helper.min.js"></script>
+        <script src="//rawgit.com/mismith/angularfire-helper/1.1.0/angularfire-helper.min.js"></script>
 
-2. Include this library in your angular application (instead of `firebase`)
+2. Include this library as a module dependency in your angular application (instead of `firebase`)
 
         angular.module('my-app', ['firebaseHelper'])
 
@@ -20,9 +22,109 @@ Helper service to simplify and improve common AngularFire tasks
         	$firebaseHelperProvider.namespace('my-app');
         })
 
-4. Include the `$firebaseHelper` factory in place of `$firebase`, e.g.
+4. Include the `$firebaseHelper` service in place of the usual `$firebase` one
 
         .controller('AppCtrl', function($scope, $firebaseHelper){
-	        // '$rootScope.myObject = $firebase(fbRef.child('myObject')).$asObject();' becomes:
+            // old
+            var ref = new Firebase('https://my-app.firebaseio.com/');
+            $rootScope.myObject = $firebase(ref.child('myObject')).$asObject();
+            // new
         	$rootScope.myObject = $firebaseHelper.object('myObject');
         })
+
+
+
+## API
+
+First, a note regarding the `[arguments…]` params below. If the first parameter is one of the following:
+
+* a `Firebase` reference,
+* a `$firebase` instance,
+* a `$FirebaseObject`, or
+* a `$FirebaseArray`,
+
+Then it will be detected as so, and the subsequent arguments will be treated as strings to be joined as a child path. The resulting node will therefore be a child of the first node. For example:
+
+    var ref  = $firebaseHelper.ref('parent'),
+    	obj1 = $firebaseHelper.object(ref, 'child1/child2', 'child3'),
+    	obj2 = $firebaseHelper.object('parent/child1/child2/child3');
+    
+    // obj1 === obj2
+
+
+### Authentication
+
+* `auth([arguments…])`
+
+    **Returns**: an Angular-augmented authentication object.
+    
+    **Replaces**: `$firebaseAuth(ref)`.
+
+
+### Data Types
+
+* `ref([arguments…])`
+
+    **Returns**: a `Firebase` reference at the API level, i.e. with no angular enhancements.
+    
+    **Replaces**: `new Firebase(path)`.
+
+
+* `inst([arguments…])`
+
+    **Returns**: a `$firebase` instance.
+    
+    **Replaces**: `$firebase(ref)`.
+    
+        // without $firebaseHelper
+        $firebase(new Firebase(baseUrl + '/' + path + '/' + subpath1 + '/' + subpath2));
+        $firebase(ref);
+        
+        // with $firebaseHelper
+        $firebaseHelper.inst(path, subpath1, subpath2);
+        $firebaseHelper.inst(ref);
+
+
+* `object([arguments…][, asArray])`
+
+    **Returns**: a `$FirebaseObject`, or, if the last parameter === `true`, then a `$FirebaseArray`.
+    
+    **Replaces**: `$firebase().$asObject()` and `$firebase().$asArray()`, respectively.
+
+
+* `array([arguments…])`
+
+    **Returns**: a `$FirebaseArray`.
+    
+    **Replaces**: shortcut for `$firebaseHelper.object([arguments…], true)`. 
+
+
+### Utility
+
+* `load([arguments…][, asArray])`
+
+    **Returns**: a promise that resolves when the required resource is ready. The first param of the callback will be that loaded resource.
+    
+    **Replaces**: shortcut for `$firebaseHelper.object([arguments…], true).$loaded()`.
+
+* `populate(keys, values, cbAdded)`
+
+    **Returns**: an array of `$FirebaseObject`s. Both `keys` and `values` params work like `[arguments…]` in that they can be strings or special Firebase objects, or arrays thereof.
+    
+    `cbAdded` is a callback function that will be called every time a `values`-linked `$FirebaseObject` is loaded (the object itself being passed as a callback param). If the callback returns a promise, the `$FirebaseObject` won't be added to the populated array until that promise is resolved. e.g.
+    
+        var delayedCount = 0;
+        $scope.populated = $firebaseHelper.populate('keys', 'values', function(loadedValueObject){
+            var deferred = $q.defer();
+            $timeout(function(){
+                delayedCount++;
+                deferred.resolve(); // `$scope.populated` now contains `loadedValueObject`
+            }, 2000);
+            return deferred.promise;
+        });
+
+* `intersect(keysPath, valuesPath[, keysMap[, valuesMap]])`
+
+    Call `Firebase.util`s more thorough (but significantly slower) populating/intersecting algorithm.
+    
+    **Requires**: external `Firebase.util` library, see: [https://github.com/firebase/firebase-util](https://github.com/firebase/firebase-util)
--- angularfire-helper.js
+++ angularfire-helper.js
@@ -1,8 +1,9 @@
 angular.module('firebaseHelper', ['firebase'])
-
+	
 	.provider('$firebaseHelper', function(){
 		var namespace = '',
-			cached    = {};
+			demo      = false,
+			cache     = {};
 			
 		// get or set namespace/Firebase reference domain
 		this.namespace = function(set){
@@ -10,77 +11,108 @@ angular.module('firebaseHelper', ['firebase'])
 			return namespace;
 		};
 		
+		// is this a demo url?
+		this.demo = function(set){
+			if(set !== undefined) demo = set;
+			return demo;
+		};
+		
+		var url = this.url = function(){
+			if( ! namespace) throw new Error('Firebase namespace not set, configure $firebaseHelperProvider.namespace(\'my-app\') before using $firebaseHelper.');
+			return 'https://' + namespace + '.firebaseio' + (demo ? '-demo' : '') + '.com/';
+		};
+		var trim = function(path){
+			return path.replace(/^\/+/, '');
+		};
+		
 		this.$get = ['$firebase', '$firebaseAuth', '$q', function($firebase, $firebaseAuth, $q){
 			var self = this;
 			
-			// $firebaseAuth wrapper
-			self.auth = function(ref){
-				if(ref === undefined) ref = self.ref(); // empty, use root by default
-				else if(angular.isString(ref)) ref = self.ref(ref); // string/path
-				else if(angular.isFunction(ref.$ref)) ref = ref.$ref; // Instance
+			// auto-detects input and turns any supported special object or string path [chunks] into a specified type
+			var $get = self.$get = function(input, as){
+				var $inst,
+					path = as;
 				
-				return $firebaseAuth(ref);
+				// first let's get everything to a Firebase Instance since we can get the others from there
+				if(angular.isString(input)){ // it's a string path
+					$inst = $firebase(new Firebase(url() + trim(input)));
+				}else if(angular.isArray(input)){ // it's an array
+					if(input.length){
+						if(angular.isString(input[0])){ // of string path chunks
+							$inst = $firebase(new Firebase(url() + trim(input.join('/'))));
+						}else{ // first item is a special object itself
+							// so let's recurse to get the first item as a Firebase Reference
+							var parent = input.shift(),
+								ref    = $get(parent, 'ref');
+							
+							// then join the remaining arguments as a path to it's child
+							ref = ref.child(trim(input.join('/')));
+							
+							// then wrap it for further processing
+							$inst = $firebase(ref);
+						}
+					}
+				}else if(angular.isObject(input)){
+					if(angular.isFunction(input.$inst)){ // it's a Firebase Object or Array
+						$inst = input.$inst();
+					}else if(angular.isFunction(input.$ref)){ // it's a Firebase Instance
+						$inst = input;
+					}else if(angular.isFunction(input.child)){ // it's a Firebase Reference
+						$inst = $firebase(input);
+					}
+				}
+				if( ! $inst){ // it's undefined or undetectable, so just use the root path
+					$inst = $firebase(new Firebase(url()));
+				}
+				
+				// let's check if we have it cached already, and return that if so
+				path += $inst.$ref().path.toString();
+				if(cache[path]) return cache[path];
+				
+				// otherwise let's cache it and return it
+				switch(as){
+					case 'ref':
+						return cache[path] = $inst.$ref();
+					default:
+					case 'inst':
+						return cache[path] = $inst;
+					case 'object':
+						return cache[path] = $inst.$asObject();
+					case 'array':
+						return cache[path] = $inst.$asArray();
+				}
 			};
+			
+			
+			
+			// $firebaseAuth wrapper
+			self.auth = function(){
+				return $firebaseAuth($get(Array.prototype.slice.call(arguments), 'ref'));
+			};
+			
 			
 			// returns: Reference
 			self.ref = function(){
-				var args = Array.prototype.slice.call(arguments);
-				
-				var path = 'Ref/' + args.join('/');
-				if(cached[path]) return cached[path];
-				
-				var $ref = new Firebase('https://' + namespace + '.firebaseio.com/' + (args.join('/') || ''));
-				cached[path] = $ref;
-				
-				return $ref;
+				return $get(Array.prototype.slice.call(arguments), 'ref');
 			};
 			
 			// returns: Instance
 			self.inst = function(){
-				if(arguments.length == 1 && arguments[0] instanceof Firebase){
-					// accept/handle firebase $ref as argument too, not just string(s)
-					var ref  = arguments[0],
-						path = 'Inst' + ref.path;
-					
-					if(cached[path]) return cached[path];
-					
-					var $inst = $firebase(ref);
-					cached[path] = $inst;
-				
-					return $inst;
-				}else{
-					// handle string(s)
-					var args = Array.prototype.slice.call(arguments),
-						path = 'Inst/' + args.join('/');
-					if(cached[path]) return cached[path];
-					
-					var $inst = $firebase(self.ref.apply(this, args));
-					cached[path] = $inst;
-				
-					return $inst;
-				}
+				return $get(Array.prototype.slice.call(arguments), 'inst');
 			};
 			
 			// returns: Object [or Array]
 			// N.B. if last argument === true, return Array instead of Object
 			self.object = function(){
 				var args = Array.prototype.slice.call(arguments),
-					type = 'Object';
+					type = 'object';
 				
 				if(args[args.length - 1] === true){
-					type = 'Array';
+					type = 'array';
 					args.pop();
 				}
 				
-				// retrieve cached item, if possible
-				var path = type + '/' + args.join('/');
-				if(cached[path]) return cached[path];
-				
-				// retrieve from remote, then cache it for later
-				var $get = self.inst.apply(this, args)['$as'+type]();
-				cached[path] = $get;
-				
-				return $get;
+				return $get(args, type);
 			};
 			self.array = function(){
 				var args = Array.prototype.slice.call(arguments);
@@ -89,32 +121,17 @@ angular.module('firebaseHelper', ['firebase'])
 				return self.object.apply(this, args);
 			};
 			
-			// returns: promise for Object or Array
+			
+			
+			// returns: promise for Object [or Array]
 			self.load = function(){
 				return self.object.apply(this, arguments).$loaded();
 			};
 			
-			// returns: Instance
-			self.child = function(){
-				var args = Array.prototype.slice.call(arguments),
-					parent = args.shift();
-				
-				if(angular.isFunction(parent.$inst)){ // it's a Firebase Object or Array
-					parent = parent.$inst();
-				}
-				if(angular.isFunction(parent.$ref)){ // it's a Firebase Instance
-					parent = parent.$ref();
-				}
-				if(angular.isFunction(parent.child)){ // it's a Firebase Reference
-					return self.inst(parent.child(args.join('/')));
-				}
-				return parent; // fallback to parent
-			};
-			
-			// populate a list of keys with the model values they reference
+			// populate a list of keys with the values they reference
 			self.populate = function(keys, values, cbAdded){
 				var array   = [],
-					keysRef = self.ref(keys);
+					keysRef = $get(keys, 'ref');
 				
 				// fire callback even if no keys found
 				keysRef.once('value', function(snapshot){
@@ -125,7 +142,7 @@ angular.module('firebaseHelper', ['firebase'])
 				
 				// watch for additions/deletions at keysRef
 				keysRef.on('child_added', function(snapshot){
-					var $item = self.object(values, snapshot.key());
+					var $item = $get([values, snapshot.key()], 'object');
 					
 					$item.$loaded().then(function(){
 						var deferreds = [];
@@ -151,7 +168,7 @@ angular.module('firebaseHelper', ['firebase'])
 			
 			// @requires: external Firebase.util library: https://github.com/firebase/firebase-util
 			self.intersect = function(keysPath, valuesPath, keysMap, valuesMap){
-				if( ! Firebase.util) throw new Error('$firebaseHelper.intersect requires Firebase.util external library. See: https://github.com/firebase/firebase-util');
+				if( ! Firebase.util) throw new Error('$firebaseHelper.intersect() requires the `Firebase.util` external library. See: https://github.com/firebase/firebase-util');
 				
 				// @TODO: cache somehow
 				
@@ -163,6 +180,7 @@ angular.module('firebaseHelper', ['firebase'])
 				
 				return $firebase(Firebase.util.intersection(keysObj, valuesObj)).$asArray();
 			};
+			//*/
 			
 			return self;
 		}];
